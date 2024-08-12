54d5b510a425aab9a193d47f82651b7323169e16
El-Chapo0133 <levequeloris66@gmail.com>
2024-08-12 16:02:15 +0200

    pushed last changes on laps

--- getline/laps.c
+++ getline/laps.c
@@ -59,17 +59,3 @@ void race_state(int *id, size_t size) {
 	print_cars(cars);
 }
 
-
-int main(void) {
-	int ids1[3] = {1, 42, 101};
-    int ids2[1] = {11};
-
-    race_state(ids1, 3);
-    printf("--\n");
-    race_state(ids1, 3);
-    printf("--\n");
-    race_state(ids1, 3);
-    printf("--\n");
-    race_state(ids2, 1);
-
-}
