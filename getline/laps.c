9915adcfb9c57f65b814de62a7831898e923a820
El-Chapo0133 <levequeloris66@gmail.com>
2024-08-12 16:25:13 +0200

    pushed last changes on laps

--- getline/laps.c
+++ getline/laps.c
@@ -1,14 +1,25 @@
 #include "laps.h"
 
-
+/**
+ * free_cars - free all alocated cars
+ * @head: head of the linked list
+ *
+ * Return: void
+ */
 void free_cars(Car *head) {
 	while (head != NULL) {
-		Car *temp = head->next;
-		free(head);
-		head = temp;
+		Car *temp = head;
+		head = head->next;
+		free(free);
 	}
 }
 
+/**
+ * print_cars - print all cars in the linked list
+ * @head: head of the linked list
+ *
+ * Return: void
+ */
 void print_cars(Car *head) {
 	printf("Race state:\n");
 	while (head != NULL) {
@@ -16,6 +27,13 @@ void print_cars(Car *head) {
 	}
 }
 
+/**
+ * insert_car - insert a can in the right place of the linked list
+ * @head: head of the linked list
+ * @id: id to insert
+ *
+ * Return: void
+ */
 void insert_car(Car *head, int id) {
 	if (head == NULL) {
 		Car *car = malloc(sizeof(Car));
