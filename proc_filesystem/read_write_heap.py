59947a917aa0d4f7d77bdddc16c8f3a5175ec751
El-Chapo0133 <levequeloris66@gmail.com>
2024-09-13 11:34:20 +0200

    Pushed last changes on proc

--- proc_filesystem/read_write_heap.py
+++ proc_filesystem/read_write_heap.py
@@ -5,7 +5,7 @@ This file looks a process by it's pid and replace a value in the heap of this pr
 
 from sys import argv
 
-def main():
+def init():
     """This is the main function"""
     if len(argv) < 3:
         print("Not enough args :(")
@@ -19,25 +19,36 @@ def main():
     # try to open the files
     try:
         maps_file = open("/proc/{}/maps".format(pid), 'r')
-        mem_file = open("/proc/{}/mem".format(pid), 'r+b', 0)
+        mem_file = open("/proc/{}/mem".format(pid), 'r+b', 0) # open with binary mode
     except Exception as e:
+        # problems :(
+        # probably not found or permissions denied
         print(e)
         maps_file.close()
         mem_file.close()
         exit(1)
+    
+    # read the maps file line by line
     for line in maps_file.readlines():
         line_splitted = line.split()
-        # once the heap line is found, get the adresses
+
+        # once the line that descrive the heap is found
         if '[heap]' in line_splitted:
+            # extract the address range
             mem_positions = line_splitted[0].split('-')
             start_position = int(mem_positions[0], 16)
             end_position = int(mem_positions[1], 16)
+
             # move to the start position and read the values
             mem_file.seek(start_position)
             string_read = mem_file.read(end_position - start_position)
+            
             # find the position of the target_string
+            # this gives us a number where the string is
             lookup_position = string_read.find(str.encode(target_string))
+
             # if there's no target_string found, break the loop
+            # usually when values is equals to -1
             if lookup_position < 0:
                 break
             else:
@@ -45,9 +56,10 @@ def main():
                 mem_file.seek(start_position + lookup_position)
                 mem_file.write(str.encode(replace_string) + b'\x00')
             break
+
     # never forget to close openned files at the end
     maps_file.close()
     mem_file.close()
 
 if __name__ == "__main__":
-    main()
+    init()
