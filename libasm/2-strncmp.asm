50664d082fdf807561a5a09fb88d4c823f5a4bf9
El-Chapo0133 <levequeloris66@gmail.com>
2024-09-27 13:28:36 +0200

    Pushed last changes

--- libasm/2-strncmp.asm
+++ libasm/2-strncmp.asm
@@ -1,27 +1,51 @@
+	BiTS 64
 		section .text
 		global asm_strncmp
 asm_strncmp:
-        mov rax, 0
-        mov rcx, 0
-loop:
-        test rdx, rdx
-        jz out
-
-        mov al, [rdi]
-        mov bl, [rsi]
-
-        cmp al, bl
-        jne diff
-
-        test al, al
-        jz out
-
-        inc rdi
-        inc rsi
-        dec rdx
-        jmp loop
-diff:
-        mov rax, 1
-        ret
-out:
+        push    rbp
+        mov     rbp, rsp
+        mov     QWORD PTR [rbp-24], rdi
+        mov     QWORD PTR [rbp-32], rsi
+        mov     DWORD PTR [rbp-36], edx
+        mov     DWORD PTR [rbp-4], 0
+        jmp     .L2
+.L6:
+        mov     eax, DWORD PTR [rbp-4]
+        movsx   rdx, eax
+        mov     rax, QWORD PTR [rbp-24]
+        add     rax, rdx
+        movzx   edx, BYTE PTR [rax]
+        mov     eax, DWORD PTR [rbp-4]
+        movsx   rcx, eax
+        mov     rax, QWORD PTR [rbp-32]
+        add     rax, rcx
+        movzx   eax, BYTE PTR [rax]
+        cmp     dl, al
+        jle     .L3
+        mov     eax, 1
+        jmp     .L4
+.L3:
+        mov     eax, DWORD PTR [rbp-4]
+        movsx   rdx, eax
+        mov     rax, QWORD PTR [rbp-24]
+        add     rax, rdx
+        movzx   edx, BYTE PTR [rax]
+        mov     eax, DWORD PTR [rbp-4]
+        movsx   rcx, eax
+        mov     rax, QWORD PTR [rbp-32]
+        add     rax, rcx
+        movzx   eax, BYTE PTR [rax]
+        cmp     dl, al
+        jge     .L5
+        mov     eax, -1
+        jmp     .L4
+.L5:
+        add     DWORD PTR [rbp-4], 1
+.L2:
+        mov     eax, DWORD PTR [rbp-4]
+        cmp     eax, DWORD PTR [rbp-36]
+        jl      .L6
+        mov     eax, 0
+.L4:
+        pop     rbp
         ret
