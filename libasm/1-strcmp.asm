2c20b179869742a20f6298627d636a2471d6b26b
root <root@LorisDesktop>
2024-09-27 14:59:20 +0200

    pushed last changes

--- libasm/1-strcmp.asm
+++ libasm/1-strcmp.asm
@@ -2,6 +2,13 @@
 		section .text
 		global asm_strcmp
 asm_strcmp:
+        cmp rdi, 0      ; check if rdi is null
+        je out
+        cmp rsi, 0      ; check if rsi is null
+        jg greater
+        je out
+        jl less
+
         mov rax, 0      ; assume it returns 0 meaning equals
         cmp rdi, rsi    ; compare 1st arg (rdi) with 2nd arg (rsi)
         je out          ; if equals, jump to out and return
@@ -9,5 +16,11 @@ asm_strcmp:
         mov rax, 1      ; then assume it returns 1 meaning not equals
         jmp out         ; jump to out and return
 
+greater:
+        mov rax, 1
+        jmp out
+less:
+        mov rax, -1
+        jmp out
 out:
         ret             ; return rax
--- libasm/2-strncmp.asm
+++ libasm/2-strncmp.asm
@@ -4,14 +4,14 @@ BITS 64
 asm_strncmp:
         mov rax, 0
 loop:
-    mov BL, BYTE [rdi + rdx]    ; store first arg char in BL
-    mov BH, BYTE [rsi + rdx]    ; store second arg char in BH
-	cmp BL, BH  ; compare them
-	jnz diff	; when not zero return 1 or -1
-	dec rdx		; dec counter
-	cmp rdx, 0	; when rdx is at zero
-	jz out		; return 0
-	jmp loop	; loop again
+        mov BL, BYTE [rdi + rdx]    ; store first arg char in BL
+        mov BH, BYTE [rsi + rdx]    ; store second arg char in BH
+        cmp BL, BH  ; compare them
+        jnz diff	; when not zero return 1 or -1
+        dec rdx		; dec counter
+        cmp rdx, 0	; when rdx is at zero
+        jz out		; return 0
+        jmp loop	; loop again
 diff:
         mov rax, 1
         ret
