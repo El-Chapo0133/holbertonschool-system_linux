6926fd9426ebabba45646a1d9c4a203e0c2ac240
root <root@LorisDesktop>
2024-09-27 14:05:18 +0200

    pushed last changes

--- libasm/0-strlen.asm
+++ libasm/0-strlen.asm
@@ -1,12 +1,12 @@
 		section .text
 		global asm_strlen
 asm_strlen:
-		mov rax, 0 ; start a counter to 0
+		mov rax, 0 				; start a counter to 0
 loop:
 		; rdi + rax mean we get the caracter, the adress + 1 equals 2nd char
 		cmp BYTE [rdi + rax], 0 ; compare 1st arg (rdi) with null-char
-		je out ; if cmp is true jump to out and return
-		inc rax ; else increment rax
-		jmp loop ; jump to loop again
+		je out 					; if cmp is true jump to out and return
+		inc rax 				; else increment rax
+		jmp loop 				; jump to loop again
 out:
 		ret
--- libasm/1-strcmp.asm
+++ libasm/1-strcmp.asm
@@ -2,12 +2,12 @@
 		section .text
 		global asm_strcmp
 asm_strcmp:
-        mov rax, 0 ; assume it returns 0 meaning equals
-        cmp rdi, rsi ; compare 1st arg (rdi) with 2nd arg (rsi)
-        je out ; if equals, jump to out and return
+        mov rax, 0      ; assume it returns 0 meaning equals
+        cmp rdi, rsi    ; compare 1st arg (rdi) with 2nd arg (rsi)
+        je out          ; if equals, jump to out and return
 
-        mov rax, 1 ; then assume it returns 1 meaning not equals
-        jmp out ; jump to out and return
+        mov rax, 1      ; then assume it returns 1 meaning not equals
+        jmp out         ; jump to out and return
 
 out:
-        ret ; return rax
+        ret             ; return rax
