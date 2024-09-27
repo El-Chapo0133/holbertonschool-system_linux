ee5a76a41aa5f79e81fde32ffb16e83b9795c091
root <root@LorisDesktop>
2024-09-27 11:34:12 +0200

    pushed last changes

--- libasm/libasm.h
+++ libasm/libasm.h
@@ -1,5 +1,11 @@
+#ifndef LIBASM
+#define LIBASM
+
 #include <stddef.h>
 
 size_t asm_strlen(const char *str);
 int asm_strcmp(const char *s1, const char *s2);
 int asm_strncmp(const char *s1, const char *s2, int n);
+
+
+#endif
