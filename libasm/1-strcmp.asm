766b6e8a1a11677eedc2eb41458f665642324842
root <root@LorisDesktop>
2024-09-27 15:00:21 +0200

    pushed last changes

--- libasm/1-strcmp.asm
+++ libasm/1-strcmp.asm
@@ -5,13 +5,13 @@ asm_strcmp:
         cmp rdi, 0      ; check if rdi is null
         je out
         cmp rsi, 0      ; check if rsi is null
-        jg greater
         je out
-        jl less
 
         mov rax, 0      ; assume it returns 0 meaning equals
         cmp rdi, rsi    ; compare 1st arg (rdi) with 2nd arg (rsi)
+        jg greater
         je out          ; if equals, jump to out and return
+        jl less
 
         mov rax, 1      ; then assume it returns 1 meaning not equals
         jmp out         ; jump to out and return
