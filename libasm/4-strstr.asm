a65fa2b86c71299f4ad7354059e9a2e3dd7acd4e
Loris Leveque <9601@holbertonstudents.com>
2024-10-04 14:25:34 +0000

    Pushed last changes

--- libasm/4-strstr.asm
+++ libasm/4-strstr.asm
@@ -2,8 +2,9 @@
 		section .text
 		global strstr
 strstr:
-	push rbp
-	mov rsp, rbp
+	push	rbp
+	mov	rbp, rsp
+	xor	rax, rax
 
 	cmp rdi, 0	; check for null
 	je out
@@ -14,9 +15,9 @@ strstr:
 	mov rcx, 0	; setup 2nd counter
 
 loop:
-	mov BL, BYTE [rdi + rdx]
-	mov BH, BYTE [rsi + rcx]
-	cmp BL, BH	; compare both char
+	mov al, BYTE [rdi + rdx]
+	mov bl, BYTE [rsi + rcx]
+	cmp al, bl	; compare both char
 	je next_step	; if equal inc counter and check for null
 	jne undo_step	; if not reset the counter
 
@@ -36,6 +37,6 @@ found:
 	mov rax, rdi + rdx - rcx; mov first char of occurence in rdi
 	jmp out
 out:
-	mov rbp, rsp
-	pop rbp
+	mov	rbp, rsp
+	pop	rbp
 	ret
