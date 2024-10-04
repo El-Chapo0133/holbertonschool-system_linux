85c039c094405c41474c6ebbc5c60bce58350b04
Loris Leveque <9601@holbertonstudents.com>
2024-10-04 12:03:14 +0000

    Pushed last changes

--- libasm/1-strcmp.asm
+++ libasm/1-strcmp.asm
@@ -2,25 +2,25 @@
 		section .text
 		global asm_strcmp
 asm_strcmp:
-        cmp rdi, 0      ; check if rdi is null
-        je out
-        cmp rsi, 0      ; check if rsi is null
-        je out
+        cmp	rdi, 0      ; check if rdi is null
+        je	out
+        cmp	rsi, 0      ; check if rsi is null
+        je	out
 
-        mov rax, 0      ; assume it returns 0 meaning equals
+        xor	rax, rax      ; assume it returns 0 meaning equals
 
 loop:
-        mov BL, [rsi]
-        mov BH, [rdi]
-        cmp rsi, rdi
-        jne not_equal
+        mov BH, [rdi]	; load char to BH
+        mov BL, [rsi]	; load char to BL
+        cmp BH, BL	; compare them
+        jne not_equal	; 
 
-        inc rdi
-        inc rsi
-        jmp out
+        inc rdi		; inc rdi to next char
+        inc rsi		; inc rsi to next char
+        jmp out		; loop again
 
 not_equal:
-        sub rdi, rsi
+        sub rdi, rsi	
         cmp rdi, 0
         jg greater
         jl less
