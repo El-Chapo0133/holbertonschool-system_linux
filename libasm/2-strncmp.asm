76b7699ed8fa6ac1859da0f0b030c76ee8800450
Loris Leveque <9601@holbertonstudents.com>
2024-10-04 09:39:08 +0000

    Pushed last changes

--- libasm/2-strncmp.asm
+++ libasm/2-strncmp.asm
@@ -6,16 +6,21 @@ asm_strncmp:
 	mov rbp, rsp
 
         mov rax, 0
+	; rdi = first string
+	; rsi = second string
+	; rdx = n char to check
 loop:
         mov BH, BYTE [rdi]    ; store first arg char in BL
         mov BL, BYTE [rsi]    ; store second arg char in BH
         cmp BH, BL	; compare them
         jne diff	; when not zero return 1 or -1
-        test BH, BH	; check for null-character
-	je end_of_str
 	inc rdi		; inc to next char
 	inc rsi		; inc to next char
-        dec rdx		; dec counter
+        test BH, BH	; check for null-character
+	je end_of_str
+        test BL, BL
+	je end_of_str
+	dec rdx		; dec counter
 	jz loop		; as long as rdx is not 0 loop again
 
 	; here means it's the end of the counter, so result=0
