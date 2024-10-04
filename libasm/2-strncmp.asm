2e1606b505da9ddd8fd4174d0d5580b3424f7b9f
Loris Leveque <9601@holbertonstudents.com>
2024-10-04 09:01:40 +0000

    Pushed last changes

--- libasm/2-strncmp.asm
+++ libasm/2-strncmp.asm
@@ -2,28 +2,29 @@ BITS 64
 		section .text
 		global asm_strncmp
 asm_strncmp:
+	push rbp
+	mov rbp rsp
         mov rax, 0
-	mov rcx, 0
 loop:
-        mov BL, BYTE [rdi + rcx]    ; store first arg char in BL
-        mov BH, BYTE [rsi + rcx]    ; store second arg char in BH
+        movzx BL, BYTE [rdi]    ; store first arg char in BL
+        movzx BH, BYTE [rsi]    ; store second arg char in BH
         cmp BL, BH	; compare them
-        jnz diff	; when not zero return 1 or -1
-        inc rcx		; dec counter
-        cmp rcx, rdx	; when rdx is at zero
-        je out		; return 0
-        jmp loop	; loop again
+        jne diff	; when not zero return 1 or -1
+        test BL, BL	; check for null-character
+	je out
+	inc rdi		; inc to next char
+	inc rsi		; inc to next char
+        dec rdx		; dec counter
+	jnz loop	; as long as rdx is not 0 loop again
+
+	; here means it's the end of the counter, so result=0
+	mov rax, 0
+	jmp out
 diff:
-	sub rdi + rcx, rsi + rcx
-	cmp rdi + rcx, 0
-	jg greater
-	jl less
-        je out
-greater:
-	mov rax, 1
-	jmp out
-less:
-	mov rax, -1
-	jmp out
+	sub rdi, rsi
+	mov rax, rdi
+        jmp out
 out:
+	mov rsp, rbp
+	pop rsp
         ret
