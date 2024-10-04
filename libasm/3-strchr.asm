411edaf79045ee0ed0b4b56abcbea40bf025d64d
Loris Leveque <9601@holbertonstudents.com>
2024-10-04 14:14:30 +0000

    Pushed last changes

--- libasm/3-strchr.asm
+++ libasm/3-strchr.asm
@@ -3,7 +3,7 @@
 		global asm_strchr
 asm_strchr:
 	push	rbp		; save current base ptr
-	mov	rsp, rbp	; establish new base ptr
+	mov	rbp, rsp	; establish new base ptr
 
 	cmp	rdi, 0		; check for null
 	je	out
@@ -16,10 +16,10 @@ loop:
 	mov	BH, BYTE [rdi]	; load current char
 	cmp	BH, BL		; compare current char with char to find
 	je	found		; jump to found when they are equal
-	
+
 	cmp	rdi, 0		; check for null-character
 	je	out		; if equal jump to out
-	
+
 	inc	rdi		; inc char
 	jmp	loop		; loop again
 
@@ -27,6 +27,6 @@ found:
 	mov	rax, rdi	; move address to rax
 	jmp	out
 out:	
-	mov	rsp, rbp	; restore the stack ptr
+	mov	rbp, rsp	; restore the stack ptr
 	pop	rbp		; restore the base ptr
 	ret			; return
