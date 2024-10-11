a1af1187d986c487f2b41eb4866a8578c7bfc07b
El-Chapo0133 <levequeloris66@gmail.com>
2024-10-11 10:51:46 +0200

    Pushed last changes

--- signals/signals.h
+++ signals/signals.h
@@ -14,7 +14,9 @@ typedef void (*sighandler_t)(int);
 
 int handle_signal(void);
 void (*current_handler_signal(void))(int);
+int handle_sigaction(void);
 void (*current_handler_sigaction(void))(int);
+int trace_signal_sender(void);
 
 
 #endif
