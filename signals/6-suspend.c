0fea096422b136449e729021b67197b4f0ac63bd
El-Chapo0133 <levequeloris66@gmail.com>
2024-10-11 11:14:40 +0200

    Pushed last changes

---
+++ signals/6-suspend.c
@@ -0,0 +1,27 @@
+#include "signals.h"
+
+
+/**
+ * sigint_handler - handler for sigint
+ * @sig_no: singal id
+ *
+ * Return: void
+ */
+void sigint_handler(int sig_no)
+{
+	printf("Caught %d\nSignal received\n", sig_no);
+}
+
+
+int main(void)
+{
+	struct sigaction s_action;
+	/* clean signals */
+	sigemptyset(&s_action.sa_mask);
+	s_action.sa_flags = 0;
+	s_action.sa_handler = sigint_handler;
+	sigaction(SIGINT, &s_action, NULL);
+	
+	pause();
+	return (0);
+}
