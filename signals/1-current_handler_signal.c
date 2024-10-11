435ebb5aad0bea18a6ff83e38f8f34f127f8bd40
El-Chapo0133 <levequeloris66@gmail.com>
2024-10-11 09:55:33 +0200

    Pushed last changes

---
+++ signals/1-current_handler_signal.c
@@ -0,0 +1,12 @@
+#include "signals.H"
+
+void (*current_handler_signal(void))(int)
+{
+	sighandler_t default_handler = signal(SIGINT, SIG_DFL);
+
+	if (default_handler == SIG_ERR)
+		return (NULL);
+	else if (signal(SIGINT, default_handler) == SIG_ERR)
+		return (NULL);
+	return (default_handler);
+}
--- signals/signals.h
+++ signals/signals.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 
 int handle_signal(void);
+void (*current_handler_signal(void))(int);
 
 
 #endif
