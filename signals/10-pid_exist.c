64029834173a4eb35ce630f9962d8900497212be
El-Chapo0133 <levequeloris66@gmail.com>
2024-10-11 11:42:39 +0200

    Pushed last changes

---
+++ signals/10-pid_exist.c
@@ -0,0 +1,12 @@
+#include "signals.h"
+
+/**
+ * pid_exist - check if a pid is running
+ * @pid: pid to check
+ *
+ * Return: 1 if exists, 0 if not
+ */ 
+int pid_exist(pid_t pid)
+{
+	return (!kill(pid, SIGINT));
+}
