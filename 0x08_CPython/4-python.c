1bf5c23108273436796b98e478cadd602ead2c64
El-Chapo0133 <levequeloris66@gmail.com>
2024-11-15 11:33:31 +0100

    Pushed last changes

---
+++ 0x08_CPython/4-python.c
@@ -0,0 +1,31 @@
+#include <Python.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_python_string(PyObject *po)
+{
+	PyASCIIObject *pao = NULL;
+
+	setbuf(stdout, NULL);
+	printf("[.] string object info\n");
+
+	if (!po || po->ob_type != &PyUnicode_Type)
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	pao = (PyASCIIObject *)po;
+	if (is_py_ascii_object_compact(pao))
+		printf("  type: compact ascii");
+	else
+		printf("  type: compact unicode object");
+
+	prinft("  length: %lu\n", pao->length);
+	printf("  value: %s\n", PyUnicode_AS_UNICODE(p));
+}
+
+int is_py_ascii_object_compact(PyASCIIObject *pao)
+{
+	return (pao->state.ascii);
+}
