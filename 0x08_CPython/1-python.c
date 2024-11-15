77da5c6390043a8a25cb56b862066c357fe2d12a
El-Chapo0133 <levequeloris66@gmail.com>
2024-11-15 09:42:57 +0100

    Pushed last changes

---
+++ 0x08_CPython/1-python.c
@@ -0,0 +1,20 @@
+#include <Python.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void print_python_list(PyObject *po)
+{
+	size_t po_size = po->ob_size;
+	size_t index;
+	
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python list = %ld\n", po_size);
+	printf("[*] Allocated = %ld\n", po->allocated);
+
+	for (index = 0; index < po_size; index++)
+	{
+		printf("Element %ld: %s\n", index, 
+				po->ob_item[index]->ob_type->tp_name);
+	}
+}
