e9fd39610094a3853fdb1cbf4df934996b43273f
El-Chapo0133 <levequeloris66@gmail.com>
2024-12-06 11:31:54 +0100

    Pushed last changes

--- strace/0-strace.c
+++ strace/0-strace.c
@@ -27,7 +27,7 @@ void trace_all_sysnums(pid_t pid)
 	waitpid(pid, &status, 0);
 
 	/* set options of pid to trace syscalls */
-	/* ptrace(PTRACE_SETOPTIONS, pid, 0, PTRACE_O_TRACESYSGOOD); */
+	ptrace(PTRACE_SETOPTIONS, pid, 0, PTRACE_O_TRACESYSGOOD);
 
 	while (1)
 	{
