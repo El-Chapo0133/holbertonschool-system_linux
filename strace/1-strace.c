d111d98a9de7b5710007e3200fb77807dd189600
El-Chapo0133 <levequeloris66@gmail.com>
2024-12-13 09:14:02 +0100

    Pushed last changes

---
+++ strace/1-strace.c
@@ -0,0 +1,66 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  0-strace.c
+ *
+ *    Description: exo 0 strace 
+ *
+ *        Version:  1.0
+ *        Created:  06.12.2024 09:23:07
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Loris Lévêque 
+ *   Organization:  
+ *
+ * ============================================================================
+ */
+
+#include "strace.h"
+
+void trace_all_sysnums(pid_t pid)
+{
+	int status;
+ 	struct user_regs_struct regs;
+
+	setbuf(stdout, NULL);
+
+	waitpid(pid, &status, 0);
+	/* set options of pid to trace syscalls */
+ 	ptrace(PTRACE_SETOPTIONS, pid, 0, PTRACE_O_TRACESYSGOOD);
+	while (1)
+	{
+		if (await_syscall(pid))
+			break;
+
+		memset(&regs, 0, sizeof(regs));
+		if (ptrace(PTRACE_GETREGS, pid, 0, &regs) != -1)
+			fprintf(stdout, "%lu\n", syscalls_64_g[(long)regs.orig_rax].name);
+
+		if (await_syscall(pid))
+			break;
+
+	}
+}
+
+
+int main(int argc, char **argv)
+{
+	pid_t pid;
+	
+	/* check args and if file argv[1] exists */
+	if (!parse_args(argc, argv))
+		return (EXIT_FAILURE);
+
+	pid = fork();
+	if (pid < 0)
+		return (EXIT_FAILURE);
+	
+	/* replace the process of pid=0 by the one given in argv[1] */
+	if (pid == 0)
+		return (replace_process(argv + 1) == -1);
+
+	trace_all_sysnums(pid);
+
+	return (EXIT_SUCCESS);
+}
