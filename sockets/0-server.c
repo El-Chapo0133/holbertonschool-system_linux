60048b9e54999761670929e3513bd5ab0ef064b7
El-Chapo0133 <levequeloris66@gmail.com>
2025-02-13 10:13:40 +0100

    Pushed last changes

---
+++ sockets/0-server.c
@@ -0,0 +1,34 @@
+/*
+ * ==========================================================================
+ *
+ *       Filename:  0-server.c
+ *
+ *    Description:  opens an IPv4/TCP socket, and listens to traffic
+ *			on port 12345
+ *
+ *        Version:  1.0
+ *        Created:  10.02.2025 12:06:19
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Loris Levêque (9601), loris@leveque.website
+ *   Organization:  Holberton
+ *
+ * ==========================================================================
+ */
+
+#include "sockets.h"
+
+
+/**
+ * main - main function
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: return code
+ */
+int main(int argc, char **argv)
+{
+	int socket_fd;
+
+}
---
+++ sockets/sockets.h
@@ -0,0 +1,31 @@
+/*
+ * ==========================================================================
+ *
+ *       Filename:  sockets.h
+ *
+ *    Description:  header file for the socket project
+ *
+ *        Version:  1.0
+ *        Created:  10.02.2025 12:03:59
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Loris Levêque (9601), loris@leveque.website
+ *   Organization:  Holberton
+ *
+ * ==========================================================================
+ */
+
+#ifndef H_SOCKETS
+#define H_SOCKETS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <apra/inet.h> /* for addr formats */
+#include <sys/socket.h>
+
+#define PORT 12345
+
+
+
+#endif
