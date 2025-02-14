b5f2898a7f437240eecb42c6ef2cd5320cb8f96f
El-Chapo0133 <levequeloris66@gmail.com>
2025-02-14 12:20:51 +0100

    Pushed last changes

--- sockets/4-request.c
+++ sockets/4-request.c
@@ -39,10 +39,12 @@ int main(void)
 int parse_request(int client_fd, char *buffer)
 {
 	char *delims = " \t\r\n";
+	char *method, *path, *version;
 
+	method = strtok(buffer, delims);
+	path = strtok(NULL, delims);
+	version = strtok(NULL, delims);
 	fprintf(stdout, "Method: %s\nPath: %s\nVersion: %s\n",
-			strtok_r(buffer, delims, &buffer),
-			strtok_r(buffer, delims, &buffer),
-			strtok_r(buffer, delims, &buffer));
+		method, path, version);
 	return (send_response(client_fd, RESPONSE_200));
 }
